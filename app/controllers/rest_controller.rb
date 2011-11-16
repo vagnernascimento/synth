1823a381b33a8e1b7dbc74e322e3c163db4dc7e3
unknown <Vagner@.(none)>
2011-11-16 00:47:38 -0200

    Testing rest for resources;

--- app/controllers/rest_controller.rb
+++ app/controllers/rest_controller.rb
@@ -8,15 +8,15 @@ class RestController < ApplicationController
     render :text => RDF::Property.find_all.to_json
   end
   
-  def show
-    render :text => RDFS::Resource.new(params[:id]).to_json
-  end
   def showddd
+    render :text => RDFS::Resource.new(params[:id]).direct_properties.to_json
+  end
+  def show
     @resource          = RDFS::Resource.new(params[:id])
     @current_class_uri = @resource.classes.first.uri
     @domain_classes    = RDFS::Class.domain_classes.sort{|a,b| a.compact_uri <=> b.compact_uri }
     @meta_classes      = RDFS::Class.meta_classes.sort{|a,b| a.compact_uri <=> b.compact_uri }
-    render :template   => 'resources/show'
+    render :template   => 'rest/show', :layout => false
   end
   
 end
