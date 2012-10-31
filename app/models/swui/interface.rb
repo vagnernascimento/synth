5c3c07f9ef3f1e5df062c0e046a8011b7bdff9ce
Vagner Nascimento <vagnernascimento@gmail.com>
2012-10-31 18:34:37 -0200

    Included Rules::Facts class to manage array facts of rules. -- Refactoring session (SWUI::Interface)

--- app/models/swui/interface.rb
+++ app/models/swui/interface.rb
@@ -1,7 +1,7 @@
-
 require "#{RAILS_ROOT}/lib/interface_rules.rb"
-require "useragent"
+
 SWUI::Interface
+
 module SWUI
 	class SWUI::Interface
 		
@@ -20,59 +20,20 @@ module SWUI
 			interfaces.sort!{ | x, y | (x.interface_weight.to_s.to_i || 0) <=> (y.interface_weight.to_s.to_i || 0) }
 		end
 		
-		# Extract facts from { :nav_element => @context, :current_node=> @node, :user_agent => request.user_agent, :environment => request.env }
-		def evaluate_selection_rule(facts_source = {})
+		# Extract facts from { :nav_element => @context, :current_node=> @node, :user_agent => request.user_agent["HTTP_USER_AGENT"], :environment => request.env }
+		# To do ===> Add params!
+		def evaluate_selection_rule(facts_triples = [])
 			
 			unless self.interface_selection_rule.empty?
 				inst_rule = InterfaceRules::InstanceRule.new( self.interface_selection_rule.to_s )
 				#== Adding facts
-				inst_rule.add_facts(facts_from_properties( facts_source[:nav_element])) #navigational_element
-				inst_rule.add_facts(facts_from_properties( facts_source[:current_node])) #current_node
-				inst_rule.add_facts(facts_from_user_agent( facts_source[:user_agent])) # User agent data
-				inst_rule.add_facts(facts_from_hash( facts_source[:environment])) #environment data
+				inst_rule.add_facts(facts_triples)
 				return inst_rule.is_true?
 			end
 		end
 		
 		private
 		
-		def facts_from_user_agent(str)
-			facts = Array.new
-			unless str.empty?
-				ua = UserAgent.parse(str)
-				facts << ["user_agent", "browser", ua.browser]
-				facts << ["user_agent", "browser_version", ua.version]
-				facts << ["user_agent", "platform", ua.platform]
-				facts << ["user_agent", "mobile", ua.mobile?]
-			end
-			return facts
-			
-		end
-
-		def facts_from_hash(hash)
-			facts = Array.new
-			hash.each {|key, value| 
-			facts << [ "environment", key, value ] } 
-			return facts
-		end
-		
-		def facts_from_properties(node)
-			facts = Array.new
-			unless node.nil?
-				node.classes.each { | prop | 
-					facts << [ node.uri.to_s, "class", prop.to_s ] 
-				}
-				node.direct_properties.each{ | prop | 
-					predicate = prop.label.first.to_s || prop.first.to_s
-					if prop.is_a?(Array)
-						prop.each{ | value |facts << [ node.uri.to_s, predicate, value.to_s ] }
-					else
-						facts << [ node.uri.to_s, predicate, prop.to_s ] 
-					end
-				}
-			end
-			return facts
-		end
 		
 	end
 end
