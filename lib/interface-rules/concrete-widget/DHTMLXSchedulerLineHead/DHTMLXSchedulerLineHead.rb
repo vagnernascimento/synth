92607a6bb96fdca2419fab97a09fd5ed2d0830b0
Vagner Nascimento <vagnernascimento@gmail.com>
2013-01-04 16:55:52 -0200

    Now the widget DHTMLXSchedulerLineHead accepts the parameter css_class

--- lib/interface-rules/concrete-widget/DHTMLXSchedulerLineHead/DHTMLXSchedulerLineHead.rb
+++ lib/interface-rules/concrete-widget/DHTMLXSchedulerLineHead/DHTMLXSchedulerLineHead.rb
@@ -3,9 +3,12 @@ class DHTMLXSchedulerLineHead < ConcreteWidget::WidgetBase
     @content  = params[:content] || ""
     @name = params[:name]
     @id = params[:id]
+		@css_class = params[:css_class]
     @params = params
+		
   end
 	def render
-		@parent.add_entry('section', content) if @parent.respond_to?(:add_entry)
+		text = super(params)
+		@parent.add_entry('section', text) if @parent.respond_to?(:add_entry)
 	end
 end
